bc21f47eda012c174fb01cc20985f9a225764fb5
haneen <haneenhariry8@gmail.com>
2024-08-16 03:54:36 +0300

    image edite

--- task1/src/data/info.jsx
+++ task1/src/data/info.jsx
@@ -3,13 +3,13 @@ export const infoData =
     {
         id: 1 ,
         href: 'mailto:info@company.com',
-        icon : '/public/imag/InfoNavImg/email.svg',
+        icon : './imag/InfoNavImg/email.svg',
         infoTitle: 'info@company.com'
     },
     {
         id: 2 ,
         href : 'https://maps.app.goo.gl/D6aSFmd72ZcWqWxH8',
-        icon : '/public/imag/InfoNavImg/map.svg',
+        icon : './imag/InfoNavImg/map.svg',
         infoTitle : 'Sunny Isles Beach,FL33160'
     }
 ]
@@ -20,25 +20,25 @@ export const infoIcon =
         id: 1 ,
         href : 'https://www.facebook.com/profile.php?id=100041919843926&mibextid=ZbWKwL',
         socIcon: 'facebook',
-        icon: '/public/imag/InfoNavImg/facebook.svg'
+        icon: './imag/InfoNavImg/facebook.svg'
     },
     {
         id: 2 ,
         href : '#',
         socIcon: 'twitter',
-        icon: '/public/imag/InfoNavImg/twitter.svg'
+        icon: './imag/InfoNavImg/twitter.svg'
     },
     {
         id: 3 ,
         href : 'https://www.linkedin.com/in/haneen-hariri6/',
         socIcon: 'linkedin',
-        icon: '/public/imag/InfoNavImg/linked.svg'
+        icon: './imag/InfoNavImg/linked.svg'
     },
     {
         id: 4 ,
         href : 'https://www.instagram.com/haneen_hariri_20?igsh=eDVwcjN4YXhuMGRn',
         socIcon: 'instagram',
-        icon: '/public/imag/InfoNavImg/instagram.svg'
+        icon: './imag/InfoNavImg/instagram.svg'
     }
 
 ]
--- task1/src/data/slidData.jsx
+++ task1/src/data/slidData.jsx
@@ -2,17 +2,17 @@ export const slidData =
 [
     {
         id : 1,
-        img: '/public/imag/HeroImg/banner-01.jpg',
+        img: './imag/HeroImg/banner-01.jpg',
         alt: 'Villa'
     },
     {
         id : 2,
-        img: '/public/imag/HeroImg/banner-02.jpg',
+        img: './imag/HeroImg/banner-02.jpg',
         alt: 'Villa'
     },
     {
         id : 3,
-        img: '/public/imag/HeroImg/banner-03.jpg',
+        img: './imag/HeroImg/banner-03.jpg',
         alt: 'Villa'
     }
 ]
